1571426f1f2efc070cb09cba7f91ddec2d17b790
Christian Dupuis <cd@atomist.com>
2019-02-07 00:47:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running. Idiot.', chalk.red('✗'));
   process.exit();
 });
 

01ca0641c22e3d4abf54ffca7f543cbec7d79131
Christian Dupuis <cd@atomist.com>
2019-02-07 14:37:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
  */
 dotenv.load({ path: '.env.example' });
 
-/**
+/** 
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
